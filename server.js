35e39f7cea20d3819f67c008a0c224418f032411
PrabhjeetBissora <C21412282@mytudublin.ie>
2025-01-17 23:49:26 +0000

    added basic code

---
+++ server.js
@@ -0,0 +1,13 @@
+// source: https://www.w3schools.com/REACT/DEFAULT.ASP
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function Hello(props) {
+  //return <h1>Hello World!</h1>;
+  console.log("Hello, this is a new JavaScript file!");
+}
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(Hello());
