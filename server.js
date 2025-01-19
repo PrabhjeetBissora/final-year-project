b8d1273eff1b727be17ae58d1c36be59c3c6eba4
PrabhjeetBissora <C21412282@mytudublin.ie>
2025-01-19 23:52:27 +0000

    ran to generate app

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 function Hello(props) {
-  //return <h1>Hello World!</h1>;
+  return <h1>Hello World!</h1>;
   console.log("Hello, this is a new JavaScript file!");
 }
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(Hello());
+root.render(<hello/>);
