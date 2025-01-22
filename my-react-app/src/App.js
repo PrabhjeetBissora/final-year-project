91a070ab95ec6245ee22920d100ecbe61feb6cf5
PrabhjeetBissora <C21412282@mytudublin.ie>
2025-01-22 23:49:19 +0000

    added google map attributes

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import GoogleMap from './GoogleMap'
 
 function App() {
   return (
@@ -18,6 +19,14 @@ function App() {
           Learn React
         </a>
       </header>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <GoogleMap/>
     </div>
   );
 }
