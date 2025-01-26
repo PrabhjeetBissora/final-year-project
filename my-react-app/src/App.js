62e084eb9f9c21b04dfd8dc6e631830caaa4d786
PrabhjeetBissora <C21412282@mytudublin.ie>
2025-01-26 23:49:01 +0000

    added google maps api loading function

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import GoogleMap from './GoogleMap'
+import React, { useEffect } from "react";
 
 function App() {
   return (
@@ -32,3 +33,26 @@ function App() {
 }
 
 export default App;
+
+const GoogleMap = () => {
+  useEffect(() => {
+    const loadGoogleMapsAPI = () => {
+      const script = document.createElement("script");
+      script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBoPryoNFGz_SvBneuhkfgIcMI381f88fQ&callback=initMap";
+      script.async = true;
+      document.body.appendChild(script);
+
+      script.onload = () => {
+        if (window.google) {
+          initMap();
+        } else {
+          console.error("Google Maps API failed to load.");
+        }
+      };
+
+      return () => {
+        document.body.removeChild(script);
+      };
+    }
+  })
+}
