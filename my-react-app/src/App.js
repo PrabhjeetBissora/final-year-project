f486855700641527bc3640ff22af44845688bee7
PrabhjeetBissora <C21412282@mytudublin.ie>
2025-01-29 23:29:30 +0000

    hardcoded working on react app now

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,38 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
-import GoogleMap from './GoogleMap'
 import React, { useEffect } from "react";
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
-}
-
-function App() {
-  return (
-    <div>
-      <GoogleMap/>
-    </div>
-  );
-}
-
-export default App;
 
 const GoogleMap = () => {
   useEffect(() => {
@@ -137,5 +103,16 @@ const GoogleMap = () => {
         }
       });
     };
-  })
+
+    loadGoogleMapsAPI();
+  }, [])
+
+  return (
+    <div>
+      <h1>My First Google Map</h1>
+      <div id="map" style={{ height: "600px", width: "100%" }}></div>
+    </div>
+  );
 }
+
+export default GoogleMap;
